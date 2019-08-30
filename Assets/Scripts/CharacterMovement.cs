be468dda0f3dd402e00a62a123e38b58abe2d92b
PhoneticNuan <54707894+PhoneticNuan@users.noreply.github.com>
2019-08-30 18:20:59 +0800

    Initial commit

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public string PlayerInput;
+    public float MoveSpeed = 0f;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis(PlayerInput);
+        transform.Translate(Vector2.up * horizontal * MoveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CharacterState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterState : MonoBehaviour
+{
+    private float health;
+
+    public void GetHurt(float damage)
+    {
+        health -= damage;
+    }
+}
